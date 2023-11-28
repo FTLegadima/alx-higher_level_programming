3a99402ee3c7626f1b1ebff14cdcc99e405a69ef
FTLegadima <67741320@mylife.unisa.ac.za>
2023-11-28 13:17:53 -0800

    0x01-python-if_else_loops_functions

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,10 +1,5 @@
 #!/usr/bin/python3
-
 for i in range(122, 96, -1):
     if i % 2 != 0:
-        # Convert the ASCII value to uppercase
         i = i - 32
     print("{}".format(chr(i)), end="")
-
-# Add a newline after the loop
-print()
