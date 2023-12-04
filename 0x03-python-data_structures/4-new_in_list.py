2ba997cc2bc2ddd5b6ed2e60e8e9f0e682062ee4
FTLegadima <67741320@mylife.unisa.ac.za>
2023-12-04 07:06:06 -0800

    0x03-python-data_structures

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    tmp_list = my_list[:]
-    if 0 <= idx < len(my_list):
-        tmp_list[idx] = element
-        return(tmp_list)
-    return(my_list)
+    # Check if idx is negative or out of range
+    if idx < 0 or idx >= len(my_list):
+        return my_list.copy()
+
+    # Create a copy of the original list and modify the element at the specified index
+    new_list = my_list.copy()
+    new_list[idx] = element
+
+    return new_list
